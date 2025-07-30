96f58bd22aa17500bde891938d10f8a7f3823a8a
Gabriela Schubert <gabrielaschubert172@gmail.com>
2025-07-30 19:43:30 -0300

    adiciona suporte a temas claro e escuro, estiliza navegação e botão de troca de tema

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function atualizarRelogio() {
 setInterval(atualizarRelogio, 1000);
 
 atualizarRelogio();
+
+function toggleTheme() {
+  const body = document.body;
+  const isDark = body.classList.contains("dark");
+
+  if (isDark) {
+    body.classList.remove("dark");
+    body.classList.add("light");
+  } else {
+    body.classList.remove("light");
+    body.classList.add("dark");
+  }
+}
